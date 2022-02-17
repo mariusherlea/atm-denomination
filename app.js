f05c429b265fe64be12aa9afad963742291564df
Marius <mariusherlea@gmail.com>
2022-02-17 13:50:12 +0200

    stucturing code into functions

--- app.js
+++ app.js
@@ -1,47 +1,55 @@
+let amount = 275;
+let billsValues = [100, 50, 20, 10, 5];
+
+function denomination(sum, billsValues) {
 
 
-let amount = 136;
+    let billsNumber = [];
+    let rest = [];
 
-let billsValues = [100, 50, 20, 10, 5, 1];
 
-let billsNumber = [];
+    printInitialValue(sum,billsValues);
 
-let rest=[];
 
-console.log('Denomination amount: ',amount);
-console.log('____________');
-for (const logEntry of billsValues) {
-    console.log(logEntry);
-}
-console.log('__________');
+    billsNumber.push(Math.floor(amount / billsValues[0]));
 
-billsNumber.push(Math.floor(amount / billsValues[0]));
+    console.log(`bill's number`, billsValues[0], "=>", billsNumber[0]);
 
-console.log(`bill's number`,billsValues[0],'=>',billsNumber[0]);
-
-rest.push(amount-(billsNumber[0]*billsValues[0]));
+    rest.push(amount - (billsNumber[0] * billsValues[0]));
 
 // console.log('restul dupa impartirea la',valoareaBancnotelor[0],'=>',rest[0]);
 
 
+    for (let i = 1; i <= billsValues.length; i++) {
 
-for (let i=1;i<=billsValues.length;i++) {
+        if (rest[i - 1] > 0 || rest < billsNumber[i]) {
 
-    if (rest[i - 1] > 0 || rest<billsNumber[i]) {
+            billsNumber.push(Math.floor(rest[i - 1] / billsValues[i]));
 
-        billsNumber.push(Math.floor(rest[i - 1] / billsValues[i]));
+            console.log(`bill's number`, billsValues[i], "=>", billsNumber[i]);
 
-        console.log(`bill's number`, billsValues[i], '=>', billsNumber[i]);
+            rest.push(rest[i - 1] - (billsNumber[i] * billsValues[i]));
 
-        rest.push(rest[i - 1] - (billsNumber[i] * billsValues[i]));
+            // console.log('restul dupa impartirea la', valoareaBancnotelor[i], '=>', rest[i]);
 
-        // console.log('restul dupa impartirea la', valoareaBancnotelor[i], '=>', rest[i]);
+        } else {
+            console.log(`rest is less than smallest bill or equal to 0`)
+            break;
+        }
+    }
 
-    } else break;
+
 }
 
+function printInitialValue(amount,billsValue){
+    console.log("Denomination amount: ", amount);
 
-
-
+    console.log("____________");
+    for (const logEntry of billsValue) {
+        console.log(logEntry);
+    }
+    console.log("__________");
+}
+denomination(amount, billsValues);
 
 
