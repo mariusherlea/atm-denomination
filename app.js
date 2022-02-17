33a8aa7db0631d573d0095ff04945dbe1a6923e7
Marius <mariusherlea@gmail.com>
2022-02-17 14:48:28 +0200

    rename var

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-let amount = 245;
+let sumToDenominate = 185;
 let billsValues = [100, 50, 20, 10, 5];
 
-let rest = [];
+let restAfterDenomination = [];
 let billsNumber = [];
 
 function denomination(sum, billsValues) {
 
-    printInitialValue(sum,billsValues);
+    printInitialValue(sumToDenominate, billsValues);
 
-    initializeArray(sum,billsValues);
-
-// console.log('restul dupa impartirea la',valoareaBancnotelor[0],'=>',rest[0]);
+    initializeArray(sumToDenominate, billsValues);
 
 
     for (let i = 1; i <= billsValues.length; i++) {
 
-        if (rest[i - 1] > 0 || rest < billsNumber[i]) {
+        if (restAfterDenomination[i - 1] > 0 || restAfterDenomination < billsNumber[i]) {
 
-            billsNumber.push(Math.floor(rest[i - 1] / billsValues[i]));
+            billsNumber.push(Math.floor(restAfterDenomination[i - 1] / billsValues[i]));
 
             console.log(`bill's number of `, billsValues[i], "=>", billsNumber[i]);
 
-            rest.push(rest[i - 1] - (billsNumber[i] * billsValues[i]));
+            restAfterDenomination.push(restAfterDenomination[i - 1] - (billsNumber[i] * billsValues[i]));
 
-            // console.log('restul dupa impartirea la', valoareaBancnotelor[i], '=>', rest[i]);
+            // console.log('restul dupa impartirea la', valoareaBancnotelor[i], '=>', restAfterDenomination[i]);
 
         } else {
             console.log(`rest is less than smallest bill or equal to 0`)
@@ -34,8 +32,8 @@ function denomination(sum, billsValues) {
 
 }
 
-function printInitialValue(amount,billsValue){
-    console.log("Denomination amount: ", amount);
+function printInitialValue(amount, billsValue) {
+    console.log("Denomination sumToDenominate: ", amount);
 
     console.log("____________");
     for (const logEntry of billsValue) {
@@ -44,18 +42,18 @@ function printInitialValue(amount,billsValue){
     console.log("__________");
 }
 
-function initializeArray(amount, billsValues){
+function initializeArray(amount, billsValues) {
 
 
     billsNumber.push(Math.floor(amount / billsValues[0]));
 
-
     console.log(`bill's number of`, billsValues[0], "=>", billsNumber[0]);
 
-    rest.push(amount - (billsNumber[0] * billsValues[0]));
+    restAfterDenomination.push(amount - (billsNumber[0] * billsValues[0]));
 
-
+// console.log('restul dupa impartirea la',valoareaBancnotelor[0],'=>',restAfterDenomination[0]);
 }
-denomination(amount, billsValues);
+
+denomination(sumToDenominate, billsValues);
 
 
